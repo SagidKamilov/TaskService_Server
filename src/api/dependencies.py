b528a1fb30cad643d5e0a4f14016154718aa8867
SagidKamilov <89615854074a@gmail.com>
2023-12-02 10:04:59 +0300

    Исправил ошибку

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,12 +1,12 @@
 from src.repositories.tasks import TasksRepository
-from src.repositories.users import UserService
+from src.repositories.users import UserRepository
 from src.services.tasks import TasksService
 from src.services.users import UserService
 
 
 def tasks_service() -> TasksService:
-    return TasksService(tasks_repo=TasksRepository())
+    return TasksService(tasks_repo=TasksRepository)
 
 
 def users_service() -> UserService:
-    return UserService(users_repo=UserService())
+    return UserService(users_repo=UserRepository)
