ef4c5b7d81de87fb2b2a052e7ff87d31ad11e54f
SagidKamilov <89615854074a@gmail.com>
2023-12-02 10:07:45 +0300

    Добавил код, который создает асинхронную задачу, чтобы создать базу данных, если ее не существует.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+import asyncio
+
 import uvicorn
 from fastapi import FastAPI
 
 from src.api.routers import all_routers
+from src.db.create_db import create_database
 
 app = FastAPI(
     title="Тест"
@@ -13,4 +16,7 @@ for router in all_routers:
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(create_database())
+
     uvicorn.run(app="main:app", reload=True)
