fbc4acaf6b6047ba97c5ba01f2e15195380ddfd8
SagidKamilov <89615854074a@gmail.com>
2023-12-02 10:12:14 +0300

    Изменил аннотацию типов

--- src/services/tasks.py
+++ src/services/tasks.py
@@ -1,18 +1,18 @@
 from typing import List
 
 from src.repositories.abstract_repositories import AbstractRepository
-from src.schemas.tasks import TaskSchemaAdd
+from src.schemas.tasks import TaskSchemaAdd, TaskSchemaGet
 
 
 class TasksService:
     def __init__(self, tasks_repo: AbstractRepository):
-        self.tasks_repo: AbstractRepository = tasks_repo
+        self.tasks_repo: AbstractRepository = tasks_repo()
 
     async def add_task(self, task: TaskSchemaAdd) -> int:
         tasks_dict = task.model_dump()
         task_id: int = await self.tasks_repo.add_one(data=tasks_dict)
         return task_id
 
-    async def get_tasks(self) -> List[int]:
+    async def get_tasks(self) -> List[TaskSchemaGet]:
         tasks = await self.tasks_repo.find_all()
         return tasks
