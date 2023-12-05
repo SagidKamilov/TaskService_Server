fd7da2a15c327abc65015f2db27b3db2c107a4c5
SagidKamilov <89615854074a@gmail.com>
2023-12-06 01:24:47 +0300

    Добавил статические методы

--- src/services/tasks.py
+++ src/services/tasks.py
@@ -1,21 +1,24 @@
 from src.schemas.tasks import TaskSchemaAdd, TaskSchemaEdit, TaskHistorySchemaAdd, TaskDeleteSchema
-from src.utils.unitofwork import IUnitOfWork
+from src.utils.unitofwork import IUnitOfWork, UnitOfWork
 
 
 class TasksService:
-    async def add_task(self, uow: IUnitOfWork, task: TaskSchemaAdd):
+    @staticmethod
+    async def add_task(uow: IUnitOfWork, task: TaskSchemaAdd):
         tasks_dict: dict = task.model_dump()
         async with uow:
             task_id = await uow.tasks.add_one(tasks_dict)
             await uow.commit()
             return task_id
 
-    async def get_tasks(self, uow: IUnitOfWork):
+    @staticmethod
+    async def get_tasks(uow: IUnitOfWork):
         async with uow:
             tasks = await uow.tasks.find_all()
             return tasks
 
-    async def edit_task(self, uow: IUnitOfWork, task_id: int, task: TaskSchemaEdit):
+    @staticmethod
+    async def edit_task(uow: IUnitOfWork, task_id: int, task: TaskSchemaEdit):
         tasks_dict: dict = task.model_dump()
         async with uow:
             await uow.tasks.edit_one(task_id, tasks_dict)
@@ -30,12 +33,14 @@ class TasksService:
             await uow.task_history.add_one(task_history_log)
             await uow.commit()
 
-    async def get_task_history(self, uow: IUnitOfWork):
+    @staticmethod
+    async def get_task_history(uow: IUnitOfWork):
         async with uow:
             history = await uow.task_history.find_all()
             return history
 
-    async def delete_task(self, uow: IUnitOfWork, task: TaskDeleteSchema):
+    @staticmethod
+    async def delete_task(uow: UnitOfWork, task: TaskDeleteSchema):
         task_dict: dict = task.model_dump()
         async with uow:
             check = await uow.tasks.delete_one(task_dict)
