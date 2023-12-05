9578a41006a3a9309a6d56562904c066c38f97f7
SagidKamilov <89615854074a@gmail.com>
2023-12-06 00:50:42 +0300

    Удалил абстракцию для работы с базой данных. Переименовал фабрику репозитория

--- src/repositories/sql_repositories.py
+++ src/repositories/sql_repositories.py
@@ -1,33 +1,8 @@
-from abc import ABC, abstractmethod
-
 from sqlalchemy import insert, select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class AbstractRepository(ABC):
-    @abstractmethod
-    async def add_one():
-        raise NotImplementedError
-
-    @abstractmethod
-    async def find_all():
-        raise NotImplementedError
-
-    @abstractmethod
-    async def edit_one():
-        raise NotImplementedError
-
-    @abstractmethod
-    async def find_one():
-        raise NotImplementedError
-
-
-    @abstractmethod
-    async def delete_one():
-        raise NotImplementedError
-
-
-class SQLOrmRepository(AbstractRepository):
+class SQLOrmRepository:
     model = None
 
     def __init__(self, session: AsyncSession):
