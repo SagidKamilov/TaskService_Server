a9d9bf000d8f7370c46dee1481edc3a067df8dcc
SagidKamilov <89615854074a@gmail.com>
2023-12-05 11:57:18 +0300

    Переработал фабрику. Добавил транзакции. Добавил асинхронные сессии

--- src/repositories/abstract_repositories.py
+++ src/repositories/abstract_repositories.py
@@ -1,34 +1,61 @@
-from typing import List
 from abc import ABC, abstractmethod
 
-from sqlalchemy import insert, select
-
-from src.models.base import session_maker
+from sqlalchemy import insert, select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AbstractRepository(ABC):
     @abstractmethod
-    async def add_one(self, data: dict):
+    async def add_one():
         raise NotImplementedError
 
     @abstractmethod
-    async def find_all(self):
+    async def find_all():
+        raise NotImplementedError
+
+    @abstractmethod
+    async def edit_one():
+        raise NotImplementedError
+
+    @abstractmethod
+    async def find_one():
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def delete_one():
         raise NotImplementedError
 
 
 class SQLOrmRepository(AbstractRepository):
     model = None
 
-    async def add_one(self, data: dict) -> int:
-        async with session_maker() as session:
-            stmt = insert(self.model).values(**data).returning(self.model.id)
-            res = await session.execute(stmt)
-            await session.commit()
-            return res.scalar_one()
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
-    async def find_all(self) -> List[int]:
-        async with session_maker() as session:
-            stmt = select(self.model)
-            res = await session.execute(stmt)
-            res = [row[0].to_read_model() for row in res.all()]
-            return res
+    async def add_one(self, data: dict):
+        stmt = insert(self.model).values(**data).returning(self.model.id)
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
+
+    async def edit_one(self, invoked_id: int, data: dict):
+        stmt = update(self.model).values(**data).filter_by(id=invoked_id).returning(self.model.id)
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
+
+    async def find_one(self, **filter_by):
+        stmt = select(self.model).filter_by(**filter_by)
+        res = await self.session.execute(stmt)
+        res = res.one().to_read_model()
+        return res
+
+    async def find_all(self):
+        stmt = select(self.model)
+        res = await self.session.execute(stmt)
+        res = [row[0].to_read_model() for row in res.all()]
+        return res
+
+    async def delete_one(self, data: dict):
+        stmt = delete(self.model).where(self.model.id == data["id"])
+        res = await self.session.execute(stmt)
+        return res.rowcount
