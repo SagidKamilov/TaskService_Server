12c3153dc0cc71f28dce68d722ae4ef577369085
SagidKamilov <89615854074a@gmail.com>
2023-12-06 22:23:32 +0300

    Конкретизировал названия параметров

--- src/api/users.py
+++ src/api/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.schemas.users import UserSchemaAdd, UserSchemaDelete, UserSchemaEdit, UserSchema
+from src.schemas.users import UserSchemaAdd, UserSchemaDelete, UserSchemaEdit, UserSchemaGet
 from src.services.users import UserService
 
 from src.api.dependencies import UOWDep
@@ -14,28 +14,28 @@ router = APIRouter(
 @router.post("")
 async def add_user(user: UserSchemaAdd, uow: UOWDep):
     user_id = await UserService().add_user(uow, user)
-    return {"user_id": user_id}
+    return user_id
 
 
 @router.get("")
 async def get_users(uow: UOWDep):
     users = await UserService().get_users(uow)
-    return users
+    return {"id": users}
 
 
-@router.delete("")
-async def delete_users(uow: UOWDep, user: UserSchemaDelete):
-    result = await UserService().delete_users(uow, user)
+@router.delete("{user_id}")
+async def delete_user(uow: UOWDep, user_id: int):
+    result = await UserService().delete_user(uow, user_id)
     return {"deleted_row": result}
 
 
-@router.patch("/{id}")
-async def edit_users(uow: UOWDep, user_id: int):
-    await UserService().edit_user(uow, user_id)
-    return {"edit": True}
+@router.patch("/{user_id}")
+async def edit_users(uow: UOWDep, user_id: int, user: UserSchemaEdit):
+    result = await UserService().edit_user(uow, user_id, user)
+    return {"user_id": result}
 
 
-@router.get("/{id}")
+@router.get("/{user_id}")
 async def get_user(uow: UOWDep, user_id: int):
     user = await UserService().get_user(uow, user_id)
     return user
