a10f3456808fb5b77df7e364a500621251068463
SagidKamilov <89615854074a@gmail.com>
2023-12-11 23:24:44 +0300

    Добавил получение пути к БД через файл конфигурации

--- src/models/base.py
+++ src/models/base.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-engine = create_async_engine("sqlite+aiosqlite:///sqlite.db")
+from src.config import DB_URL
+
+engine = create_async_engine(DB_URL)
 session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
